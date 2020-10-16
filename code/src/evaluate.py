52f6493c72caee1d9218fa938126c14735cf254d
AndrewHamster <andrewhamster@hotmail.com>
2020-10-16 16:06:11 +0300

    Better model

--- code/src/evaluate.py
+++ code/src/evaluate.py
@@ -2,4 +2,4 @@ from pathlib import Path
 import json
 
 if __name__ == "__main__":
-    Path("metrics.json").write_text(json.dumps({'rmse': 10}))
+    Path("metrics.json").write_text(json.dumps({'rmse': 9}))
